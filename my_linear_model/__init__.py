cef02e15f13562ce7d321d50455741ea41216ce7
yofc_project <371517587@qq.com>
2019-01-10 15:58:21 +0800

    add GPRegressor

--- my_linear_model/__init__.py
+++ my_linear_model/__init__.py
@@ -1,4 +1,4 @@
-from .gaussian_discriminat_analysis import MyGaussianDiscriminatAnalysis
+from .my_gaussian_discriminat_analysis import MyGaussianDiscriminatAnalysis
 from .my_linear_regression import MyLinearRegression, MyRidge, MyLasso, MyGaussianProcessRegressor
 from .my_logistic_regression import MyLogisticRegression
 
