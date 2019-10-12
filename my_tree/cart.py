3226bf505df03cea0b7cd09ab72d57ad2f5a1417
yofc_project <371517587@qq.com>
2019-10-12 16:35:05 +0800

    add cart

--- my_tree/cart.py
+++ my_tree/cart.py
@@ -78,7 +78,7 @@ class DecisionTreeClassifier(BaseTree):
     def _choose_best_feature_value(self, X, y):
         best_feature, best_value = None, None
         best_gain = 0
-        if len(y) <= self.min_samples_split:
+        if len(y) < self.min_samples_split:
             return best_feature, best_value
         gini = self._calc_gini(y)
         for f in range(X.shape[1]):
@@ -92,18 +92,56 @@ class DecisionTreeClassifier(BaseTree):
         return best_feature, best_value
 
 
-class DecisionTreeRegressor:
-    pass
+class DecisionTreeRegressor(BaseTree):
+    def __init__(self, max_depth=5, min_samples_split=2, min_samples_leaf=1):
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.min_samples_leaf = min_samples_leaf
+        super().__init__()
 
+    def fit(self, X, y):
+        self._depth = 0
+        self._tree = self._create_tree(X, y)
 
-if __name__ == '__main__':
-    from sklearn.datasets import make_classification
+    def predict(self, X):
+        result = self._predict(X)
+        return np.column_stack(result).ravel()
 
-    X, y = make_classification(n_features=5, n_informative=5, n_redundant=0, random_state=2019, n_classes=3)
-    cart = DecisionTreeClassifier(max_depth=20)
-    cart.fit(X, y)
+    def _create_tree(self, X, y, depth=0):
+        best_feature, best_value = self._choose_best_feature_value(X, y)
+        if depth > self._depth:
+            self._depth = depth
 
-    from sklearn.metrics import accuracy_score
+        if best_feature == None or self._depth >= self.max_depth:
+            leaf = np.mean(y)
+            tree = {'leaf': leaf}
+        else:
+            left_X, right_X, left_y, right_y = self._split_data(X, y, best_feature, best_value)
+            tree = {'feature': best_feature, 'value': best_value}
+            tree['left'] = self._create_tree(left_X, left_y, depth + 1)
+            tree['right'] = self._create_tree(right_X, right_y, depth + 1)
+        return tree
 
-    print(cart._depth)
-    print(accuracy_score(y, cart.predict(X)))
+    def _calc_mse(self, y):
+        return np.sum((y - np.mean(y)) ** 2)
+
+    def _split_data(self, X, y, feature_index, value):
+        left_X = X[X[:, feature_index] < value]
+        right_X = X[X[:, feature_index] >= value]
+        left_y = y[X[:, feature_index] < value]
+        right_y = y[X[:, feature_index] >= value]
+        return left_X, right_X, left_y, right_y
+
+    def _choose_best_feature_value(self, X, y):
+        best_feature, best_value = None, None
+        best_mse = float('inf')
+        if len(y) < self.min_samples_split:
+            return best_feature, best_value
+        for f in range(X.shape[1]):
+            for v in np.unique(X[:, f]):
+                left_X, right_X, left_y, right_y = self._split_data(X, y, f, v)
+                cur_mse = self._calc_mse(left_y) + self._calc_mse(right_y)
+                if cur_mse < best_mse and np.min([len(left_y), len(right_y)]) >= self.min_samples_leaf:
+                    best_mse = cur_mse
+                    best_feature, best_value = f, v
+        return best_feature, best_value
